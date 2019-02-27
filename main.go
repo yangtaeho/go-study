8803a25642fcd3c201579cc4dcb780feeb5850e6
yangtaeho <yangbuk84@gmail.com>
2019-02-27 14:13:03 +0900

    e

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
 
 var print = fmt.Println
 
 func main() {
-    print("Hello World")
+	print("Hello World")
 
-    // 일시 정지를 위해
-    var b []byte = make([]byte, 1)
-    os.Stdin.Read(b)
-} 
+	// 일시 정지를 위해
+	var b []byte = make([]byte, 1)
+	os.Stdin.Read(b)
+}
