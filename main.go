0dfd0463b6f698e48a98d1a3c56add25471df408
yangbuk84 <yangbuk84@gmail.com>
2019-02-27 13:30:30 +0900

    ee

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+var print = fmt.Println
+
+func main() {
+    print("Hello World")
+
+    // 일시 정지를 위해
+    var b []byte = make([]byte, 1)
+    os.Stdin.Read(b)
+}
